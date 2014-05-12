2cd1689a7f52e17c27f73a488abe08f4e10ad7d8
Julia Evans <julia@jvns.ca>
2014-05-12 14:45:57 -0400

    try to use mmap, it doesn't work

---
+++ bytesum_mmap.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int main(int argc, char** argv) {
+    if (argc < 1) {
+        printf("Too few arguments!");
+        return 0;
+    }
+    const char *memblock;
+    int fd;
+    struct stat sb;
+
+    fd = open(argv[1], O_RDONLY);
+    fstat(fd, &sb);
+    uint64_t filesize = sb.st_size;
+    printf("Size: %lu\n", filesize);
+
+    memblock = mmap(NULL, filesize, PROT_WRITE, MAP_SHARED, fd, 0);
+    if (memblock == MAP_FAILED) {
+        printf("oh no it failed\n");
+        return 0;
+    }
+    uint64_t i;
+    char result;
+    for (i = 0; i < filesize; i++) {
+        result += memblock[i];
+    }
+    printf("The answer is: %d\n", result);
+    return 0;
+}
+
